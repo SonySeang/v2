f9ae58a105f89de9a2079fa66cc037d8ca77ef50
Hades-Evil <sengleeheng4@gmail.com>
2025-01-07 18:09:53 +0700

    Hello pek see

--- src/components/home/search.tsx
+++ src/components/home/search.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+'use client'
+import React, { use } from "react";
 import { Input } from "@/components/ui/input";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function Search() {
-    
-    return <Input className="w-[500px]" placeholder="Search" />;
+    const router = useRouter();
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+        const q = (form[0] as HTMLInputElement).value.trim();
+        if (!q) return;
+        router.push(`/search?q=${encodeURIComponent(q)}`);
+    }
+
+    return (
+        <form onSubmit={handleSubmit} method="GET" action="/search">
+            <Input type="text" name="q" placeholder="Search..." className="pe-10" />
+            <button type="submit"></button>
+        </form>
+    );
 }
